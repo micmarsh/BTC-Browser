2a1476f46ff0fed7da6817cdb4faa31307871f71
micmarsh <mike@marsh.pw>
2015-07-18 11:12:49 -0400

    use external lib

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [mutils "0.1.0-SNAPSHOT"]
                  [clj-http "1.1.2"]])
--- src/btc_browser/core.clj
+++ src/btc_browser/core.clj
@@ -1,35 +1,9 @@
-(ns btc-browser.core)
+(ns btc-browser.core
+  (:refer-clojure :exclude [tree-seq])
+  (:require [mutils.seq.tree :refer [tree-seq]]))
 
-(defn lazy-mapcat [f coll]
-  (for [item coll x (f item)] x))
-
-(def lazy-concat
-  (comp (partial lazy-mapcat identity) (fn [& args] args)))
-
-(defn bf-tree-seq
-  "Returns a lazy sequence of the nodes in a tree, via a breadth-first walk.
-   branch? must be a fn of one arg that returns true if passed a node
-   that can have children (but may not).  children must be a fn of one
-   arg that returns a sequence of the children. Will only be called on
-   nodes for which branch? returns true. Root is the root node of the
-  tree."
-  [branch? children root]
-  (let [walk (fn walk [prev-nodes]
-               (lazy-seq
-                (let [branches (filter branch? prev-nodes)
-                       kids (lazy-mapcat children branches)]
-                  (lazy-concat kids (walk kids)))))]
-    (cons root (walk [root]))))
-
-(defn- tree-seq-path* [tree branch? children root]
-  (tree (comp branch? :node)
-        (fn [{:keys [node path]}]
-          (map (partial hash-map :path (conj path node) :node)
-               (children node)))
-        {:node root :path []}))
-
-(def tree-seq-path (partial tree-seq-path* tree-seq))
-(def bf-tree-seq-path (partial tree-seq-path* bf-tree-seq))
+(def ^:const bf-path
+  {:breadth true :path true})
 
 (defn path?
   ([step addr-from addr-to]
@@ -39,7 +13,7 @@
            branch? (fn [_] (< (swap! cutoff inc) limit))
            children (comp step :address)]
        (->>  {:address addr-from}
-             (bf-tree-seq-path branch? children)
+             (tree-seq bf-path branch? children)
              (drop-while (comp (partial not= addr-to) :address :node))
              (first)
              (:path)))))
